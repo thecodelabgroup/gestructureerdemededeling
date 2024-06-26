bc79e1eacc9067665ffb1ccc524cf68b2b2aef60
hursittarcan <hursittarcan@gmail.com>
2024-06-26 23:46:50 +0200

    pre modulo 97 checkpoint

--- gestructureerde-mededeling-app/src/App.tsx
+++ gestructureerde-mededeling-app/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import {
   Button,
-  ChakraProvider,
+  ChakraProvider, extendTheme,
   Heading,
-  HStack,
+  HStack, Kbd,
   PinInput,
   PinInputField,
   Tab,
@@ -13,12 +13,42 @@ import {
   Tabs
 } from '@chakra-ui/react'
 import Header from "./components/Header.tsx";
-import {CopyIcon, EmailIcon} from "@chakra-ui/icons";
+import {CopyIcon} from "@chakra-ui/icons";
 
 function App() {
 
+  const theme = extendTheme({
+    components: {
+      Tabs: {
+        baseStyle: {
+          tab: {
+            _selected: {
+              color: '#3902BF',
+              borderColor: '#3902BF',
+            },
+          },
+        },
+      },
+      PinInput: {
+        baseStyle: {
+          field: {
+            _focus: {
+              color: '#3902BF',
+              borderColor: 'red',
+              boxShadow: '0 0 0 1px red',
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const pinInputOnChange = (value: string) => {
+    console.log(value);
+  }
+
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={ theme }>
         <>
           <Header style={{ boxShadow: "var(--chakra-shadows-sm)"}}/>
           <div style={{ display: "flex", alignItems: "center", paddingTop: '100px', flexDirection: "column" }}>
@@ -30,22 +60,24 @@ function App() {
                 De mededeling bestaat uit drie groepen cijfers, gescheiden door schuine strepen. De eerste groep heeft drie cijfers, de tweede vier, en de derde vijf.
                 De mededeling begint en eindigt met drie plustekens (+++).
                 <br/><br/>
-                Met onze tool kun je eenvoudig je gestructureerde mededelingen creëren, genereren en controleren.
+                Met onze tool kun je eenvoudig je gestructureerde mededelingen creëren, laten genereren en controleren.
               </p>
             </div>
             <br/>
             <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start", fontFamily: 'Poppins, sans-serif', color: "#00044F" }}>
               <Tabs variant='enclosed' style={{width: "1000px"}}>
                 <TabList>
-                  <Tab>Zelf creëren</Tab>
-                  <Tab>Automatisch genereren</Tab>
-                  <Tab>Mededeling controleren</Tab>
+                  <Tab style={{ color: "#00044F" }}><b>Zelf creëren</b></Tab>
+                  <Tab><b>Automatisch genereren</b></Tab>
+                  <Tab><b>Mededeling controleren</b></Tab>
                 </TabList>
                 <TabPanels>
                   <TabPanel>
-                    Start met het typen van een getal van maximaal 10 cijfers, de laatste twee cijfers (het controlegetal) wordt automatisch berekend.
+                    <p style={{ fontFamily: 'Poppins, sans-serif', color: "#00044F" }}>
+                      Start met het typen van een getal van maximaal 10 cijfers, de laatste twee cijfers (het controlegetal) wordt automatisch berekend.
+                    </p>
                     <HStack style={{ marginTop: "20px", padding: "20px", display: "flex", justifyContent: "center", alignItems: "center", border: "solid 1px black", borderRadius: "5px", background: "#f8c9ba"}}>
-                      <PinInput type='number' size="lg">
+                      <PinInput type='number' size="lg" onChange={pinInputOnChange} placeholder="0">
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
@@ -61,16 +93,18 @@ function App() {
                         <PinInputField/>
                         <PinInputField/>
                         <PinInputField/>
-                        <PinInputField/>
-                        <PinInputField/>
+                        <PinInput type='number' size="lg" placeholder="0" isDisabled defaultValue="00">
+                          <PinInputField/>
+                          <PinInputField/>
+                        </PinInput>
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
                         <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
                       </PinInput>
                     </HStack>
                     <br/>
-                    <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                      <Button leftIcon={<CopyIcon />} style={{ background: "#00044F", color: "white" }} variant='solid'>
+                    <div style={{display: "flex", justifyContent: "flex-end"}}>
+                      <Button leftIcon={<CopyIcon/>} style={{background: "#00044F", color: "white"}} variant='solid'>
                         Mededeling kopiëren
                       </Button>
                     </div>
@@ -89,4 +123,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
