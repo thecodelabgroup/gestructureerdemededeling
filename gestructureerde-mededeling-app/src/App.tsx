c6a715e025f0bb7ed52883578f305456231d511f
hursittarcan <hursittarcan@gmail.com>
2024-06-28 23:02:25 +0200

    add template to tab 2

--- gestructureerde-mededeling-app/src/App.tsx
+++ gestructureerde-mededeling-app/src/App.tsx
@@ -161,13 +161,44 @@ function App() {
                     </div>
                   </TabPanel>
                   <TabPanel>
-                    <p>two!</p>
+                    <p style={{fontFamily: 'Poppins, sans-serif', color: "#00044F"}}>
+                      Onderstaande mededeling werd automatisch gegenereerd met een willekeurige cijfercombinatie.
+                    </p>
+                    <HStack style={{ marginTop: "20px", padding: "20px", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "var(--chakra-radii-md)", background: "#f8c9ba"}}>
+                    <PinInput type='number' size="md" onChange={pinInputOnChange} placeholder="0" defaultValue="00000000000">
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>/</p>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>/</p>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInputField sx={customPinInputFieldStyles}/>
+                      <PinInput type='number' size="md" placeholder="0" isDisabled value={checksum}>
+                        <PinInputField sx={customPinInputFieldStyles}/>
+                        <PinInputField sx={customPinInputFieldStyles}/>
+                      </PinInput>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                      <p style={{fontSize: '28px', fontWeight: '300'}}>+</p>
+                    </PinInput>
+                  </HStack>
+                  </TabPanel>
+                  <TabPanel>
+                    Hello
                   </TabPanel>
                 </TabPanels>
               </Tabs>
             </div>
           </div>
-          <Footer />
+          <Footer/>
         </>
       </ChakraProvider>
   )
