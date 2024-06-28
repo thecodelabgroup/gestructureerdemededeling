a18590d9c5a64dfaa9ae0933d9c9ca548c761702
hursittarcan <hursittarcan@gmail.com>
2024-06-28 22:18:22 +0200

    update sub-header text

--- gestructureerde-mededeling-app/src/App.tsx
+++ gestructureerde-mededeling-app/src/App.tsx
@@ -101,8 +101,7 @@ function App() {
             <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
               <Heading as='h1' size='2xl' noOfLines={1} style={{ paddingBottom: "30px", color: "#00044F", fontFamily: 'Poppins, sans-serif' }}>Gestructureerde Mededeling.</Heading>
               <p style={{ width: "1000px", color: "#00044F", fontFamily: 'Poppins, sans-serif' }}>
-                Een overschrijving met gestructureerde mededeling, vaak afgekort als OGM, is een term uit het Belgische financiële verkeer.
-                Deze mededeling wordt gebruikt voor de automatische verwerking van betalingen.
+                Een overschrijving met gestructureerde mededeling wordt gebruikt voor de automatische verwerking van betalingen.
                 De mededeling bestaat uit drie groepen cijfers, gescheiden door schuine strepen. De eerste groep heeft drie cijfers, de tweede vier, en de derde vijf.
                 De mededeling begint en eindigt met drie plustekens (+++).
                 <br/><br/>
