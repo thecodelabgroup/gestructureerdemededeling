6fdcb9bc873f7b1606dfa0f816114417f3e294b6
hursittarcan <hursittarcan@gmail.com>
2024-07-03 21:59:53 +0200

    add i18n file

---
+++ gestructureerde-mededeling-app/src/i18n.tsx
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        debug: true,
+        interpolation: {
+            escapeValue: false
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+    });
+
+export default i18n;
