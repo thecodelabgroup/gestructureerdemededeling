4af3895dc53195fb21da008035bffa8982340813
hursittarcan <hursittarcan@gmail.com>
2024-07-05 23:48:52 +0200

    add translations for statement landing

--- gestructureerde-mededeling-app/src/components/StatementLandingText.tsx
+++ gestructureerde-mededeling-app/src/components/StatementLandingText.tsx
@@ -1,15 +1,16 @@
 import {Heading} from "@chakra-ui/react";
+import {useTranslation} from "react-i18next";
 
 export const StatementLandingText = () => {
+    const {t} = useTranslation();
+
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
             <Heading as='h1' size='2xl' noOfLines={1} style={{paddingBottom: "30px", color: "#00044F", fontFamily: 'Poppins, sans-serif'}}>
-                Gestructureerde Mededeling.
+                {t('statement-landing-main-title-label')}
             </Heading>
             <p style={{width: "1000px", color: "#00044F", fontFamily: 'Poppins, sans-serif'}}>
-                Een overschrijving met gestructureerde mededeling wordt gebruikt voor de automatische verwerking van betalingen.
-                De mededeling bestaat uit drie groepen cijfers, gescheiden door schuine strepen. De eerste groep heeft
-                drie cijfers, de tweede vier, en de derde vijf. De mededeling begint en eindigt met drie plustekens (+++).
+                {t('statement-landing-main-text-label')}
             </p>
         </div>
     )
