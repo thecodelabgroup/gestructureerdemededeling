c6b8e6c370e4dc10afc133dcacb5bb7cf69ce929
hursittarcan <hursittarcan@gmail.com>
2024-07-05 23:31:37 +0200

    update footer logo size

--- gestructureerde-mededeling-app/src/components/Footer.tsx
+++ gestructureerde-mededeling-app/src/components/Footer.tsx
@@ -5,8 +5,8 @@ export const Footer = (props: StackProps) => (
     <VStack as='footer' spacing={4} mt={12} textAlign='center' {...props}>
         <Text fontSize='sm' style={{ fontFamily: 'Poppins, sans-serif', color: "#00044F" }}>
             <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                <span style={{ marginBottom: 10, fontSize: "1rem" }}>Proudly made by</span>
-                <img src={Logo} style={{maxHeight: "20px", maxWidth: "100px"}} alt="Logo"/>
+                <span style={{ marginBottom: 5 }}>Proudly made by</span>
+                <img src={Logo} style={{maxHeight: "20px", maxWidth: "80px"}} alt="Logo"/>
             </div>
         </Text>
     </VStack>
