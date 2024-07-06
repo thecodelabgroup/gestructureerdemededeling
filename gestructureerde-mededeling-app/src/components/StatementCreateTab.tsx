b5e13c08f978f544aa82e45f16b416fe64809caf
hursittarcan <hursittarcan@gmail.com>
2024-07-07 00:32:23 +0200

    update tab one + add reset functionality

--- gestructureerde-mededeling-app/src/components/StatementCreateTab.tsx
+++ gestructureerde-mededeling-app/src/components/StatementCreateTab.tsx
@@ -9,18 +9,26 @@ import {useTranslation} from "react-i18next";
 import {useCopyStatementToClipboard} from "../hooks/UseCopyStatementToClipboard.tsx";
 
 export const StatementCreateTab = () => {
-    const [mededeling, setMededeling] = useState("+++000/0000/00000+++");
+    const [statement, setStatement] = useState("+++000/0000/00000+++");
+    const [statementValue, setStatementValue] = useState("00000000000");
     const [checksum, setChecksum] = useState("00");
     const copyStatementToClipboard = useCopyStatementToClipboard();
     const { t } = useTranslation();
 
     const pinInputOnChange = (value: string) => {
+        setStatementValue(value);
         setChecksum(calculateChecksum(value.slice(0, -1)));
-        setMededeling(statementTransformer(value.slice(0, -1)));
+        setStatement(statementTransformer(value.slice(0, -1)));
     }
 
     const copyStatementClick = () => {
-        copyStatementToClipboard(mededeling);
+        copyStatementToClipboard(statement);
+    }
+
+    const resetStatementClick = () => {
+        setStatementValue("00000000000");
+        setChecksum("00");
+        setStatement("+++000/0000/00000+++");
     }
 
     return (
@@ -29,7 +37,7 @@ export const StatementCreateTab = () => {
                 {t('create-statement-main-message')}
             </p>
             <HStack className="statement-container">
-                <PinInput type='number' size="md" onChange={pinInputOnChange} placeholder="0" defaultValue="00000000000">
+                <PinInput type='number' size="md" onChange={pinInputOnChange} placeholder="0" defaultValue="00000000000" value={statementValue}>
                     {generateStatementSegment(3, 'p', { className: 'statement-pin', children: '+' })}
                     {generateStatementSegment(3, PinInputField, { sx: statementPinInputStyles })}
                     {generateStatementSegment(1, 'p', { className: 'statement-pin', children: '/' })}
@@ -44,7 +52,7 @@ export const StatementCreateTab = () => {
             </HStack>
             <br/>
             <div className="statement-button-container">
-                <Button leftIcon={<RepeatIcon/>} className="statement-reset-button" style={{ border: "#00044F 2px solid" }} variant='outline'>
+                <Button leftIcon={<RepeatIcon/>} className="statement-reset-button" style={{ border: "#00044F 2px solid" }} variant='outline' onClick={resetStatementClick}>
                     {t('common-statement-reset-button-label')}
                 </Button>
                 <Button leftIcon={<CopyIcon/>} style={{ background: "#00044F", color: "white" }} variant='solid' onClick={copyStatementClick}>
