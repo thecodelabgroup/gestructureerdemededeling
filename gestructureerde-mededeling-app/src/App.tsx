2b6f37af812f7b4731670c5ab1f983ff689df58e
hursittarcan <hursittarcan@gmail.com>
2024-07-07 20:34:18 +0200

    update app.tsx + css for mobile view

--- gestructureerde-mededeling-app/src/App.tsx
+++ gestructureerde-mededeling-app/src/App.tsx
@@ -1,4 +1,4 @@
-import {ChakraProvider} from '@chakra-ui/react'
+import {Box, ChakraProvider, useMediaQuery} from '@chakra-ui/react'
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import {StatementLandingText} from "./components/StatementLandingText.tsx";
@@ -7,21 +7,29 @@ import {customChakraTheme} from "./styles/CustomChakraTheme.tsx";
 import {StatementTabs} from "./components/StatementTabs.tsx";
 
 function App() {
-  return (
-      <ChakraProvider theme={ customChakraTheme }>
-        <>
-          <Header style={{ boxShadow: "var(--chakra-shadows-sm)"}} />
-          <div className="main-statement-landing-container">
-            <StatementLandingText />
-            <br/>
-            <div className="main-statement-tabs-container">
-              <StatementTabs />
-            </div>
-          </div>
-          <Footer className="footer" />
-        </>
-      </ChakraProvider>
-  )
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+    return (
+        <ChakraProvider theme={ customChakraTheme }>
+            <>
+                <Header style={{ boxShadow: "var(--chakra-shadows-sm)"}} />
+                {isMobile ? (
+                    <Box textAlign="center" padding="20px">
+                        <p>This app is specifically made for desktop.</p>
+                    </Box>
+                ) : (
+                    <div className="main-statement-landing-container">
+                        <StatementLandingText />
+                        <br />
+                        <div className="main-statement-tabs-container">
+                            <StatementTabs />
+                        </div>
+                    </div>
+                )}
+                <Footer className="footer"/>
+            </>
+        </ChakraProvider>
+    );
 }
 
 export default App;
