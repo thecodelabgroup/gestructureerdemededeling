73181208eb6ee8109b5b9ced8ed840d0d4d41a42
hursittarcan <hursittarcan@gmail.com>
2024-07-08 19:38:04 +0200

    fix build errors

--- gestructureerde-mededeling-app/src/components/Header.tsx
+++ gestructureerde-mededeling-app/src/components/Header.tsx
@@ -6,39 +6,35 @@ import {
     IconButton,
     Link,
     chakra,
-    useColorModeValue,
     useDisclosure,
     useUpdateEffect, MenuButton, Menu, MenuList, MenuItem, Image
 } from '@chakra-ui/react'
 import { useScroll } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
-import { FaMoon, FaSun, FaYoutube } from 'react-icons/fa'
+import { FaYoutube } from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 import VersionSwitcher from './VersionSwitcher.tsx'
 import { GithubIcon } from './icons/GithubIcon.tsx'
 import { DiscordIcon } from './icons/DiscordIcon.tsx'
 import SponsorButton from "./SponsorButton.tsx";
 import i18n from "i18next";
-import {ChevronDownIcon} from "@chakra-ui/icons";
 
 function HeaderContent() {
     const mobileNav = useDisclosure()
     const [selectedLang, setSelectedLang] = useState('en');
 
-    const flags = {
+    const flags: Record<'nl' | 'fr' | 'en' | 'de', string> = {
         nl: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/svg/1f1f3-1f1f1.svg',
         fr: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/svg/1f1eb-1f1f7.svg',
         en: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/svg/1f1ec-1f1e7.svg',
         de: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/svg/1f1e9-1f1ea.svg',
     };
 
-    const changeLanguage = (lng: string) => {
+    const changeLanguage = (lng: 'nl' | 'fr' | 'en' | 'de') => {
         i18n.changeLanguage(lng);
         setSelectedLang(lng);
     };
 
-    const text = useColorModeValue('dark', 'light')
-    const SwitchIcon = useColorModeValue(FaMoon, FaSun)
     const mobileNavBtnRef = useRef<HTMLButtonElement>()
 
     useUpdateEffect(() => {
@@ -113,13 +109,13 @@ function HeaderContent() {
                     </HStack>
                     <HStack spacing='5'>
                         <Menu>
-                            <MenuButton ml={{ base: '0', md: '3' }} as={IconButton} variant="outline" icon={<Image src={flags[selectedLang]} alt={`${selectedLang} flag`} boxSize='20px' />}>
+                            <MenuButton ml={{ base: '0', md: '3' }} as={IconButton} variant="outline" icon={<Image src={flags[selectedLang as 'nl' | 'fr' | 'en' | 'de']} alt={`${selectedLang} flag`} boxSize='20px' />}>
                                 Language
                             </MenuButton>
                             <MenuList minWidth='50px'>
                                 {Object.keys(flags).map((lng) => (
-                                    <MenuItem key={lng} onClick={() => changeLanguage(lng)} style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-                                        <Image src={flags[lng]} alt={`${lng} flag`} boxSize='20px' />
+                                    <MenuItem key={lng} onClick={() => changeLanguage(lng as 'nl' | 'fr' | 'en' | 'de')}  style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
+                                        <Image src={flags[lng as 'nl' | 'fr' | 'en' | 'de']} alt={`${lng} flag`} boxSize='20px' />
                                     </MenuItem>
                                 ))}
                             </MenuList>
