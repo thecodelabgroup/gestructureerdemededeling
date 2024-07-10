1ead87ba70e535d00b9fcbfa60dc231a1c7c9a2d
hursittarcan <hursittarcan@gmail.com>
2024-07-10 21:52:08 +0200

    footer class cleanup

--- gestructureerde-mededeling-app/src/components/Footer.tsx
+++ gestructureerde-mededeling-app/src/components/Footer.tsx
@@ -4,12 +4,12 @@ import Logo from '../assets/logo.png'
 export const Footer = (props: StackProps) => (
     <VStack as='footer' spacing={4} mt={12} textAlign='center' {...props}>
         <Text fontSize='sm'>
-            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                <span style={{ marginBottom: 5 }}>Proudly made by</span>
-                <img src={Logo} style={{maxHeight: "20px", maxWidth: "80px"}} alt="Codelab logo"/>
+            <div className="footer-container">
+                <span className="footer-text">Proudly made by</span>
+                <img src={Logo} className="footer-logo" alt="Codelab logo"/>
             </div>
         </Text>
     </VStack>
 )
 
-export default Footer
+export default Footer;
