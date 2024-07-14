a971a4da62b1b9963d9947625f7e65184533974e
hursittarcan <hursittarcan@gmail.com>
2024-07-14 22:05:37 +0200

    update header + version switcher

--- gestructureerde-mededeling-app/src/components/Header.tsx
+++ gestructureerde-mededeling-app/src/components/Header.tsx
@@ -3,7 +3,7 @@ import {useScroll} from 'framer-motion'
 import {useEffect, useRef, useState} from 'react'
 import {FaYoutube} from 'react-icons/fa'
 import Logo from '../assets/logo.png'
-import VersionSwitcher from './VersionSwitcher.tsx'
+import {VersionSwitcher} from './VersionSwitcher.tsx'
 import {GithubIcon} from './icons/GithubIcon.tsx'
 import {DiscordIcon} from './icons/DiscordIcon.tsx'
 import SponsorButton from "./SponsorButton.tsx";
--- gestructureerde-mededeling-app/src/components/VersionSwitcher.tsx
+++ gestructureerde-mededeling-app/src/components/VersionSwitcher.tsx
@@ -1,11 +1,12 @@
 import { Select, SelectProps } from '@chakra-ui/react'
 
-export const VersionSwitcher = (props: SelectProps) => (
-    <Select marginEnd='0rem' variant='outline' fontWeight='semibold' color='gray.600' background='chakra-body-bg' sx={{ '--select-bg': 'colors.chakra-body-bg' }} focusBorderColor='#3902BF' {...props}>
-        <option>
-            v1.2.7
-        </option>
-    </Select>
-)
+export const VersionSwitcher = (props: SelectProps) => {
+    return (
+        <Select marginEnd='0rem' variant='outline' fontWeight='semibold' color='gray.600' background='chakra-body-bg' sx={{ '--select-bg': 'colors.chakra-body-bg' }} focusBorderColor='#3902BF' {...props}>
+            <option>
+                v1.2.7
+            </option>
+        </Select>
+    )
+}
 
-export default VersionSwitcher;
