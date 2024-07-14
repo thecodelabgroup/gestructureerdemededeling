da928f447bbc9aef315bf50969f6239c101f4e0a
hursittarcan <hursittarcan@gmail.com>
2024-07-14 15:50:10 +0200

    sponsor button cleanup + add translation

--- gestructureerde-mededeling-app/src/components/SponsorButton.tsx
+++ gestructureerde-mededeling-app/src/components/SponsorButton.tsx
@@ -1,40 +1,24 @@
 import { Box, BoxProps, Icon } from '@chakra-ui/react'
 import {FaHeart} from 'react-icons/fa'
+import {useTranslation} from "react-i18next";
 
-const SponsorButton = ({ onClick, ...props }: BoxProps & { onClick?: () => void }) => (
-    <Box
-        display={{ base: 'none', lg: 'flex' }}
-        alignItems='center'
-        as='button'
-        aria-label='Sponsor Chakra UI on Open Collective'
-        onClick={onClick}
-        bg='gray.50'
-        borderWidth='1px'
-        borderColor='gray.200'
-        px='1em'
-        minH='36px'
-        borderRadius='md'
-        fontSize='sm'
-        color='gray.800'
-        outline='0'
-        transition='all 0.3s'
-        _hover={{
-            bg: 'gray.100',
-            borderColor: 'gray.300',
-        }}
-        _active={{
-            borderColor: 'gray.200',
-        }}
-        _focus={{
-            boxShadow: 'outline',
-        }}
-        {...props}
-    >
-        <Icon as={FaHeart} w='4' h='4' color='red.500' mr='2' />
-        <Box as='strong' lineHeight='inherit' fontWeight='semibold'>
-            Sponsor
+const SponsorButton = ({ onClick, ...props }: BoxProps & { onClick?: () => void }) => {
+    const { t } = useTranslation();
+
+    return (
+        <Box display={{ base: 'none', lg: 'flex' }} alignItems='center' as='button' onClick={onClick} bg='gray.50' borderWidth='1px' borderColor='gray.200' px='1em' minH='36px' borderRadius='md' fontSize='sm' color='gray.800' outline='0' transition='all 0.3s' _hover={{ bg: 'gray.100', borderColor: 'gray.300' }} _active={{ borderColor: 'gray.200' }} _focus={{ boxShadow: 'outline' }} {...props}>
+            <Icon
+                as={FaHeart}
+                w='4'
+                h='4'
+                color='red.500'
+                mr='2'
+            />
+            <Box as='strong' lineHeight='inherit' fontWeight='semibold'>
+                {t('sponsor-button-label')}
+            </Box>
         </Box>
-    </Box>
-)
+    )
+}
 
-export default SponsorButton
+export default SponsorButton;
